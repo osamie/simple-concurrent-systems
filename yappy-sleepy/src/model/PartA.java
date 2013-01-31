e0b03a33e4e216d1b78e21a3f4482e9c02015c35
Osazuwa Omigie <osamie2002@gmail.com>
2013-01-30 21:26:19 -0500

    Refactor + in-code documentation

--- yappy-sleepy/src/model/PartA.java
+++ yappy-sleepy/src/model/PartA.java
@@ -1,9 +1,41 @@
 /*
- * Basic Java thread concurrency  
+ * BASIC JAVA THREAD CONCURRENCY EXPERIMENT  
  * Two thread classes will both loop for the given number of times, and each time 
  * through the loop, it will print out its one-letter name. 
  * 
- * Author: Osazuwa Omigie
+ 	OUTPUTS:
+ 		main thread output: "DONE"
+ 		sleepy thread: 's'
+ 		yappy thread: 'y'
+ 		
+    OBSERVATIONS:
+ 1. 
+ 	-Run your program. What got printed out first? 
+ 	Which of the two created threads ran first? 
+ 	
+ 	The first output was 'y'. YappyThread.start() was called before sleepyThread.start(). 
+ 	The first output 'y' was from the yappy thread.  
+ 	
+ 	
+ 	-What was the pattern of the numbers printed out?
+ 	"ysDONEyyyyyyysyyyyyyys" or "yDONEsyyyyyyysyyyyyyys"
+ 	yappy thread always output first, but sleepy and the parent interchange for 
+ 	second place.   
+ 
+ 2. 
+ 	Run your program several more times. Is the same thread always run first?
+    The same thread is always the first to run.
+    
+ 3. 
+	Run your program several more times, changing the sleep time and the spin time. What 
+	changes do you see?
+	By decreasing the spin time, the yappy thread prints to the console more frequently.
+	By increasing the sleep time, the sleepy thread prints to the console less frequently.      
+
+During the yappy thread's spin time, the thread is been 'executed' or CPU bound (doing nothing).
+Since the I/O is not in use at this time, the other thread is able use I/O in printing (if it is not asleep).  
+ 
+ Author: OSAZUWA OMIGIE
  */
 
 package model;
@@ -13,12 +45,11 @@ public class PartA {
 	
 	public void createThreads(){
 		
-		Yappy yappyThread = new Yappy();
-		Sleepy sleepyRunnable = new Sleepy();
-		
-		new Thread(sleepyRunnable).start();
+		Yappy yappyThread = new Yappy('y',99999999);
+		Sleepy sleepyRunnable = new Sleepy('s',1000);
 		
 		yappyThread.start(); //initialize thread resources and run/execute thread
+		new Thread(sleepyRunnable).start();
 		System.out.print("DONE");
 	}
 	public static void main(String[] args) 
@@ -27,14 +58,23 @@ public class PartA {
 		partA.createThreads(); //create the sleepy and yappy threads
 	}
 	
+	
+	/*Thread classes*/
+	
 	class Sleepy implements Runnable{
+		int sleepTime;
+		char output;
+		public Sleepy(char c, int delay){
+			this.sleepTime = delay;
+			this.output = c;
+		}
 		
 		@Override
 		public void run() {
 			while(1==1){
-				System.out.print("s");
+				System.out.print(output);
 				try {
-					Thread.sleep(500);
+					Thread.sleep(sleepTime);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -44,15 +84,22 @@ public class PartA {
 	}
 	
 	class Yappy extends Thread{
+		int loops;  //spin count
+		char output;  //output character 
+		
+		public Yappy(char c, int loopCount) {
+			this.loops = loopCount; //set the spin count
+			this.output = c;
+		}
 		
 		@Override
 		public void run() {
 			while(1==1){
-				System.out.print("y");
+				System.out.print(output);
 						
 				/*spin this thread*/
 				int var;
-				for(var=2;var<99999999;var++){
+				for(var=2;var<loops;var++){
 					var *=1;
 					var /=1;
 				}
--- yappy-sleepy/src/model/PartB.java
+++ yappy-sleepy/src/model/PartB.java
@@ -1,8 +1,10 @@
-/* Basic Java thread concurrency
+/* 
+ * BASIC JAVA THREAD CONCURRENCY EXPERIMENT
  * The thread classes will now simply "log" its name, instead of the 
  * expensive system call involved in printing to the console. 
  * The log is a simple global variable shared by the two threads. 
  * The parent thread would also wait for termination of the child threads
+ 	Author: OSAZUWA OMIGIE
  */
 package model;
 
@@ -18,8 +20,8 @@ public class PartB {
 	public static int logIndex = 0;
 	
 	public void createThreads(){
-		Yappy yappyThread = new Yappy();
-		Thread sleepyThread = new Thread(new Sleepy());
+		Yappy yappyThread = new Yappy('y',15);
+		Thread sleepyThread = new Thread(new Sleepy('s',5));
 		
 		try {
 			//initialize thread resources and run/execute thread
@@ -41,20 +43,29 @@ public class PartB {
 		partB.createThreads(); //create and execute the sleepy and yappy threads
 	
 		
-		System.out.print("Log: " + new String(log));
+		System.out.println("Log: " + new String(log));
 	}
 	
 	
-	/*  Inner classes */
+	
+	/*  Thread classes */
+	
 	class Sleepy implements Runnable{
+		int loops;
+		char output;
+		
+		public Sleepy(char c, int loops){
+			this.loops = loops;
+			this.output = c;
+		}
 		
 		@Override
 		public void run() {
-			int i = 10;
+			int i = loops;
 			while(i > 0){
-				log[logIndex++] = 's'; //write to log file
+				log[logIndex++] = output; //write to log file
 				try {
-					Thread.sleep(500);
+					Thread.sleep(500); //sleep for 500 milliseconds
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -64,12 +75,19 @@ public class PartB {
 	}
 	
 	class Yappy extends Thread{
+		int loops; //number of output prints 
+		char output;  //output character
+		
+		public Yappy(char c, int loopCount) {
+			this.loops = loopCount;
+			this.output = c;
+		}
 		
 		@Override
 		public void run() {
-			int i = 100;
+			int i = loops;
 			while(i > 0){
-				log[logIndex++] = 'y'; //write to log file 
+				log[logIndex++] = output; //write to log file 
 				/*spin this thread*/
 				int var;
 				for(var=2;var<99999999;var++){
